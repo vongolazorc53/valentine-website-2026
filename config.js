f1e38b3b8d04f4586a5367648bd077a8e98c8fe8
vongolazorc53 <vongolazorc53@gmail.com>
2026-02-13 20:23:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/track/6tHVEMyRfxGgQuXRzl2yOF?si=89da7291d18342db", // Music streaming URL
+        musicUrl: "https://soundcloud.com/oliviaadean/ive-seen-it", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
